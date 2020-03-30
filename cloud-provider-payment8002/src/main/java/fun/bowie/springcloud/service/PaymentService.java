5a7fe530bd45624a7a2790408b198a5a2ade1002
bowie <1250613746@qq.com>
2020-03-30 21:19:56 +0800

    微服务提供者8002

---
+++ cloud-provider-payment8002/src/main/java/fun/bowie/springcloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package fun.bowie.springcloud.service;
+
+import fun.bowie.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public  Payment getPaymentById(@Param("id") Long id);
+}
